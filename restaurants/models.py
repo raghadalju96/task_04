e193473b294848432c8bc7f5d7cfc5643659f4b0
raghadalju96 <raghadalj@Raghads-MacBook-Pro.local>
2019-07-23 16:59:29 +0300

    task4 done

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurants(models.Model):
+	name = models.CharField(max_length=120)
+
+
+	description= models.TextField()
+	opening_time=models.TimeField()
+	closing_time=models.TimeField()
+
+
+	def __str__(self):
+		return self.name
+
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
